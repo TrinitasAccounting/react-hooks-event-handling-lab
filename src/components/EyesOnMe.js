3e4e89defa3ef6a2c01f384203a2d2485bcbde31
Clay Mangum <clay@trinitasaccounting.com>
2024-02-18 14:07:17 -0500

    completed

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,23 @@
 // Code EyesOnMe Component Here
+
+function EyesOnMe() {
+
+    function focus(event) {
+        console.log('Good!')
+    };
+
+    function blur(event) {
+        console.log('Hey! Eyes on me!')
+    };
+
+
+
+    return (
+        <div>
+            <button onFocus={focus} onBlur={blur}>Eyes on me</button>
+        </div>
+    );
+}
+
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,19 @@
 // Code Keypad Component Here
 
-function Keypad (){
+function Keypad() {
+
+    function inputEventListener(event) {
+        console.log('Entering password...')
+    };
+
+
     return (
-        <div></div>
+        <div>
+            <input
+                type="password"
+                onChange={inputEventListener}
+            />
+        </div>
     )
 }
 
